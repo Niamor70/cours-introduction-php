2f8e246d180356bc8f72254bfbcdd2bb03cf5835
Romain Virmaux <niamor@zliton.com>
2016-11-15 10:41:09 +0100

    syntaxe

---
+++ 01-syntaxe.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Cours PHP</title>
+	<meta name="description" content="Cours PHP - Introduction">
+	<meta name="author" content="Romain Virmaux">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="style.css" type="text/css">
+</head>
+
+<body>
+
+<?php
+
+// un fichier php peut contenir du code HTML, du CSS, du JavaScript et du PHP, le code php doit être encadré par : <?php et ? >
+
+// Commentaires
+//
+// Ligne de commentaire simple
+
+# Une autre ligne de commentaire simple
+
+/*
+Multiples lignes de commentaires
+Multiples lignes de commentaires
+Multiples lignes de commentaires
+*/
+
+?>
+
+</body>
+</html>
